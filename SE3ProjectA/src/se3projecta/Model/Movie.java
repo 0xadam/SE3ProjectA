ce35ffc6fee413984f0d5896a709d8487c737ba9
Russell <Russell@192.168.1.100>
2013-03-07 18:09:01 +1030

    added mostly empty Model package and classes.

---
+++ SE3ProjectA/src/se3projecta/Model/Movie.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se3projecta.Model;
+
+/**
+ *
+ * @author Russell
+ */
+public class Movie {
+    private String movieName;
+    
+    
+}
---
+++ SE3ProjectA/src/se3projecta/Model/SeatType.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se3projecta.Model;
+
+/**
+ *
+ * @author Russell
+ */
+public class SeatType {
+    
+}
