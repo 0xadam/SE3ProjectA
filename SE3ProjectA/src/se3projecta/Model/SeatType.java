56f9e582581a7c63a839f6e6432ca46a6c848ee8
Russell <Russell@192.168.1.100>
2013-03-11 00:48:12 +1030

    Added xml seat type loading

--- SE3ProjectA/src/se3projecta/Model/SeatType.java
+++ SE3ProjectA/src/se3projecta/Model/SeatType.java
@@ -4,10 +4,81 @@
  */
 package se3projecta.Model;
 
+import java.io.File;
+import java.util.TreeMap;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 /**
  *
  * @author Russell
  */
 public class SeatType {
+    private String name;
+    private double price;
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "SeatType: ID:" + this.id + "\tName: " + this.name + "\tPrice: " + this.price;
+    }
     
+    public static TreeMap<String,SeatType> load(String path) {
+        TreeMap<String, SeatType> list = new TreeMap<String, SeatType>();
+        try {
+        File fXmlFile = new File(path);
+	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	Document doc = dBuilder.parse(fXmlFile);
+ 
+	NodeList nList = doc.getElementsByTagName("SeatType");
+        
+        for (int temp = 0; temp < nList.getLength(); temp++) {
+ 
+		Node nNode = nList.item(temp);
+ 
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+ 
+			Element eElement = (Element) nNode;
+                        SeatType st = new SeatType();
+                        st.setId(eElement.getAttribute("ID"));
+                        st.setName(eElement.getAttribute("Name"));
+                        st.setPrice(Double.parseDouble(eElement.getAttribute("Price")));
+                        list.put(st.getId(), st);
+		}
+	}
+        
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return list;
+    }
 }
--- SE3ProjectA/src/se3projecta/SE3ProjectA.java
+++ SE3ProjectA/src/se3projecta/SE3ProjectA.java
@@ -4,16 +4,23 @@
  */
 package se3projecta;
 
+import java.util.TreeMap;
+import se3projecta.Model.*;
 /**
  *
  * @author Tobias
  */
 public class SE3ProjectA {
-
+    private static TreeMap<String, SeatType> SeatTypes;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        SeatTypes = SeatType.load("Data/SeatTypes.xml");
+        for (int i = 0; i < SeatTypes.size(); i++) {
+            System.out.println(SeatTypes.values().toArray(new SeatType[1])[i].toString());
+        }
     }
 }
