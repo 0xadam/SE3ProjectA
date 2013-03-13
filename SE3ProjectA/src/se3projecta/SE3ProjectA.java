5e398e54aaa1fef3d2b5e5c1a0110b3bb838b540
TV <TV@129.96.82.138>
2013-03-13 12:32:44 +1030

    Enabled GUI code in main and removed unneeded code from GUI

--- SE3ProjectA/src/se3projecta/SE3ProjectA.java
+++ SE3ProjectA/src/se3projecta/SE3ProjectA.java
@@ -32,7 +32,7 @@ public class SE3ProjectA {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
-        /*
+        
          try {
          for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
          if ("Nimbus".equals(info.getName())) {
@@ -41,26 +41,26 @@ public class SE3ProjectA {
          }
          }
          } catch (ClassNotFoundException ex) {
-         java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+         java.util.logging.Logger.getLogger(se3projecta.GUI.GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
          } catch (InstantiationException ex) {
-         java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+         java.util.logging.Logger.getLogger(se3projecta.GUI.GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
          } catch (IllegalAccessException ex) {
-         java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+         java.util.logging.Logger.getLogger(se3projecta.GUI.GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
          } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-         java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+         java.util.logging.Logger.getLogger(se3projecta.GUI.GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
          }
-         */
+         
 
         //</editor-fold>
         /* Create and display the form */
-        /*
+        
          java.awt.EventQueue.invokeLater(new Runnable() {
          public void run() {
-         new GUI(control).setVisible(true);
+         new se3projecta.GUI.GUI(control).setVisible(true);
                 
          }
          });
-         */
+         
     }
 
     public SE3ProjectA() {
