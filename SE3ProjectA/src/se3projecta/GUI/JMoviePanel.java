3947b9639c1c513b9f4de2774888035ed6122552
Adam Rigg <radx@live.com.au>
2013-03-14 23:52:06 +1030

    promoImage changes with selected item

--- SE3ProjectA/src/se3projecta/GUI/JMoviePanel.java
+++ SE3ProjectA/src/se3projecta/GUI/JMoviePanel.java
@@ -11,6 +11,7 @@ import javax.imageio.ImageIO;
 import java.io.*;
 import se3projecta.Model.*;
 import java.util.Collection;
+import java.awt.event.*;
 
 /**
  *
@@ -21,45 +22,59 @@ import java.util.Collection;
  */
 public class JMoviePanel extends javax.swing.JPanel {
 
+    JLabel label = new JLabel();
+
+    //need to error check all uses of label
     public JMoviePanel(Collection<Movie> movies, Collection<SessionTime> sessionTimes) {
         //set layout
         this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-
         //create items
         JPanel dropdowns = new JPanel();
         dropdowns.setLayout(new BoxLayout(dropdowns, BoxLayout.Y_AXIS));
         JComboBox movieDropdown = new JComboBox();
+        movieDropdown.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label.setIcon(loadPromoImage(((Movie) ((JComboBox) e.getSource()).getSelectedItem()).getPromotionalImage()));
+
+            }
+        });
         JComboBox sessionTimeDropdown = new JComboBox();
         JComboBox theatreDropdown = new JComboBox();
-        JLabel label = null;
-
         //add movies to combobox
         for (Movie movie : movies) {
             movieDropdown.addItem(movie);
         }
-
         //add sessionTimes to combobox
         for (SessionTime session : sessionTimes) {
             sessionTimeDropdown.addItem(session);
         }
+        label.setPreferredSize(new Dimension(100, 150));
 
-        //load image
-        File promoImage = new File(((Movie) movieDropdown.getSelectedItem()).getPromotionalImage());
+        dropdowns.add(movieDropdown);
+
+        dropdowns.add(sessionTimeDropdown);
+
+        dropdowns.add(theatreDropdown);
+
+        add(dropdowns);
+
+        add(label);
+    }
+
+    private ImageIcon loadPromoImage(String promoImageURI) {
+        ImageIcon pic = null;
+        File promoImage = new File(promoImageURI);
         if (!promoImage.exists()) {
             promoImage = new File("data\\noimage.jpg");
         }
         try {
             BufferedImage movieImage = ImageIO.read(promoImage);
-            ImageIcon pic = new ImageIcon(movieImage);
-            label = new JLabel(pic);
-            label.setPreferredSize(new Dimension(100, 150));
+            pic = new ImageIcon(movieImage);
         } catch (IOException e) {
             System.out.println("Unable to load image.");
+        } finally {
+            return pic;
         }
-        dropdowns.add(movieDropdown);
-        dropdowns.add(sessionTimeDropdown);
-        dropdowns.add(theatreDropdown);
-        add(dropdowns);
-        add(label);
     }
 }
