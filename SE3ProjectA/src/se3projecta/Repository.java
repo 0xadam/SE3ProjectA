5be3cf51a0a331a61405e5feab6e0b4d558201e0
Tobias Wooldridge <tobias@wooldridge.id.au>
2013-03-14 16:33:56 +1030

    Reverted

--- SE3ProjectA/src/se3projecta/Repository.java
+++ SE3ProjectA/src/se3projecta/Repository.java
@@ -121,8 +121,6 @@ public class Repository {
         for (TheatreSession ts : theatreSessions.values()) {
             ts.loadRelations(theatres, movies, sessionTimes);
         }
-        
-        // TODO remove following line
         theatreSessions.get(0).getSeats()[0].setState(Seat.State.Occupied);
 
         //other initialisation code
