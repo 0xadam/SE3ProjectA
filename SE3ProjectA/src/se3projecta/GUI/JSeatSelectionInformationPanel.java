ce782a9a43070089324d0ac36e5842c5098d55c0
Adam Rigg <radx@live.com.au>
2013-03-18 23:21:08 +1030

    Fix spelling mistakes. Now displays placeholders for seats remaining.

--- SE3ProjectA/src/se3projecta/GUI/JSeatSelectionInformationPanel.java
+++ SE3ProjectA/src/se3projecta/GUI/JSeatSelectionInformationPanel.java
@@ -4,6 +4,7 @@
  */
 package se3projecta.GUI;
 
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -16,9 +17,35 @@ public class JSeatSelectionInformationPanel extends JPanel {
 
     Transaction transaction;
     JLabel remainingGoldLabel, remainingSilverLabel, remainingBronzeLabel;
-    JTextField remainingGoldTextField, remainingSilverTextField, remainingBrozeTextField;
+    JTextField remainingGoldTextField, remainingSilverTextField, remainingBronzeTextField;
 
     public JSeatSelectionInformationPanel(Transaction transaction_) { //can probably pass the specifically needed data here
         transaction = transaction_;
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        remainingGoldLabel = new JLabel("Gold Seats Left To Allocate");
+        remainingGoldTextField = new JTextField("0");
+        JPanel remainingGoldPanel = new JPanel();
+        remainingGoldPanel.setLayout(new BoxLayout(remainingGoldPanel, BoxLayout.Y_AXIS));
+        remainingGoldPanel.add(remainingGoldLabel);
+        remainingGoldPanel.add(remainingGoldTextField);
+        add(remainingGoldPanel);
+
+        remainingSilverLabel = new JLabel("Silver Seats Left To Allocate");
+        remainingSilverTextField = new JTextField("0");
+        JPanel remainingSilverPanel = new JPanel();
+        remainingSilverPanel.setLayout(new BoxLayout(remainingSilverPanel, BoxLayout.Y_AXIS));
+        remainingSilverPanel.add(remainingSilverLabel);
+        remainingSilverPanel.add(remainingSilverTextField);
+        add(remainingSilverPanel);
+
+        remainingBronzeLabel = new JLabel("Bronze Seats Left To Allocate");
+        remainingBronzeTextField = new JTextField("0");
+        JPanel remainingBronzePanel = new JPanel();
+        remainingBronzePanel.setLayout(new BoxLayout(remainingBronzePanel, BoxLayout.Y_AXIS));
+        remainingBronzePanel.add(remainingBronzeLabel);
+        remainingBronzePanel.add(remainingBronzeTextField);
+        add(remainingBronzePanel);
+
     }
 }
