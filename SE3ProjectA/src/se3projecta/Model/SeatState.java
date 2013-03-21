7930bf847a7a4e54dcadca83c0f58eeaf61fcafa
Tim <Tim@129.96.81.195>
2013-03-21 20:38:33 +1030

    Model JavaDoc

--- SE3ProjectA/src/se3projecta/Model/SeatState.java
+++ SE3ProjectA/src/se3projecta/Model/SeatState.java
@@ -10,7 +10,18 @@ package se3projecta.Model;
  */
 public enum SeatState {
 
+    /**
+     * A Seat with SeatState Empty currently has no booking and may be booked or
+     * allocated.
+     */
     Empty,
+    /**
+     * A Seat with SeatState Held has been allocated but not booked, and may be 
+     * unallocated.
+     */
     Held,
+    /**
+     * A Seat with SeatState Occupied has been booked and may not be unbooked.
+     */
     Occupied
 }
