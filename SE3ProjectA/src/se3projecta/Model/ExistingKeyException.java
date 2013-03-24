aa2c445eca218187aa70e549c4895de3db0c65fa
Adam Rigg <radx@live.com.au>
2013-03-24 22:15:11 +1030

    Refactor ExistingKeyException

--- SE3ProjectA/src/se3projecta/Model/ExistingKeyException.java
+++ SE3ProjectA/src/se3projecta/Model/ExistingKeyException.java
@@ -1,15 +1,14 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package se3projecta.Model;
 
 /**
- * Exception class for exception thrown when an element is inserted into a 
- * collection that already contains the key that is the same as that of the new 
+ * Exception class for exception thrown when an element is inserted into a
+ * collection that already contains the key that is the same as that of the new
  * element.
  *
- * @author Russell
+ * @author Timothy Moore <moor0330@flinders.edu.au>
+ * @author Russell Peake <peak0042@flinders.edu.au>
+ * @author Adam Rigg <rigg0035@flinders.edu.au>
+ * @author Tobias Wooldridge <wool0114@flinders.edu.au>
  */
 public class ExistingKeyException extends Exception {
 
