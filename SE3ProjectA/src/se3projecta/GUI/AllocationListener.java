10ce57f4ffab15f7bdd084cc59c3b76044fc7e6a
Adam Rigg <radx@live.com.au>
2013-03-24 15:08:39 +1030

    Added authors to AllocationListener

--- SE3ProjectA/src/se3projecta/GUI/AllocationListener.java
+++ SE3ProjectA/src/se3projecta/GUI/AllocationListener.java
@@ -1,13 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package se3projecta.GUI;
 
 import java.util.EventListener;
 
 /**
  *
+ * @author Timothy Moore <moor0330@flinders.edu.au>
+ * @author Russell Peake <peak0042@flinders.edu.au>
+ * @author Adam Rigg <rigg0035@flinders.edu.au>
  * @author Tobias Wooldridge <wool0114@flinders.edu.au>
  */
 public abstract class AllocationListener implements EventListener {
